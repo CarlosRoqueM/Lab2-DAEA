ad0be66da201191cefb99a338559cef4825f8cd9
Alumno <Alumno@LAB1504-04>
2024-03-23 12:50:59 -0500

    Agregar archivos de proyecto.

---
+++ Lab2/Login.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contraseña = txtPassword.Password;
+
+            if (Validar(usuario, contraseña)){
+
+                Menu menu = new Menu();
+                menu.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña invalidos");
+            }
+        }
+
+        private bool Validar(string usuario, string contraseña)
+        {
+           
+                return  usuario.ToLower() == "carlos" && contraseña.ToLower() == "123456";
+          
+            
+        }
+
+        private void SalirBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Lab2/Menu.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Lógica de interacción para Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void OperacionesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Operaciones operaciones = new Operaciones();
+            operaciones.Show();
+            Close();
+        }
+
+        private void MantenimientoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Mantenimiento mantenimiento = new Mantenimiento();
+            mantenimiento.Show();
+            Close();
+        }
+
+        private void ReportesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Reportes reportes = new Reportes();
+            reportes.Show();
+            Close();
+        }
+
+        private void SalirBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            Close();
+        }
+    }
+}
